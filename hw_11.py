72bf946583f175fb6a1393be9a982b61096c8fb7
YehupovSerhii <0676337867s@gmail.com>
2023-01-04 13:26:18 +0200

    homework_11

--- hw_11.py
+++ hw_11.py
@@ -1,84 +1,38 @@
-# hw11
-# Написать функцию которая принимает целое число - number.
-# Функция должна возвращать 'yes' если number является степенью двойки, иначе 'no'.
-# Запрещено пользоваться функцией или оператором возведение в степень.
+# 11.
+# Написать функцию, которая возвращает случайную строку заданной длины.
+# Строка должна состоять из больших и маленьких латинских букв и цифр.
+# def get_random_string(length: int) -> str:
+#   pass
+# Ограничения:
+# - Не использовать модуль string
+# - Не создавать руками список ['a', 'b', 'c', ..., 'X', 'Y', 'Z', 0, 1, ..., 8, 9]
 
-# Пример:
-
-# is_power_of_two(256) # 'yes' потому что 2 в 8 степени это 256
-
-# is_power_of_two(125) # 'no' потому что это не степень двойки
-
-# вариант 1
-
-def power_2(number):
-    if number <= 0:
-        return (False)
+import random
+def get_random_string(length: int) -> str:
+    small_letters = list(map(chr, range(97, 123)))
+    big_letters = list(map(lambda char: char.upper(), small_letters))
+    digits = list(range(0, 9))
+    total = small_letters + big_letters + digits
+    # print(type(total))
+    # n=int(input())
+    string_1 = ""
+    for i in range(0, length):
+        k = random.choice(total)
+        string_1 = string_1 + str(k)
+    # print(string_1)
+    return string_1
+while True:
+    try:
+        n = float(
+            input("Введите длину строки для вывода случайных больших и маленьких букв латинского алфавита и цифр, n: "))
+    except:
+        print('Введите число!')
     else:
-        if number & (number - 1):
-            return (False)
+        if float.is_integer(n) and n > 0:
+            n = int(n)
+            break
         else:
-            return (True)
+            print("Пробуйте еще раз ")
 
+print(f"Случайная строка, заданной длины: ", get_random_string(n))
 
-n = 0
-k = 1
-number = int(input("Введите число number для проверки - является ли число number степенью двойки:"))
-
-k = int(power_2(number))
-if k == False:
-
-    print("number(", number, ") # 'no' потому что это не степень двойки")
-else:
-    while k < number:
-        k = k * 2
-        n += 1
-    if k == number:
-        print("number(", number, ") # 'yes' потому что 2 в ", n, " степени это  ", number)
-
-
-# вариант 2
-def power_2(number):
-    k = 1
-    if number <= 0:
-        return False
-    while k <= number:
-        if k == number:
-            return True
-        k = k * 2
-    else:
-        return False
-
-
-number = int(input("Введите число number для проверки - является ли число number степенью двойки:"))
-result = power_2(number)
-
-if result == True:
-    print('Число является степенью двойки')
-else:
-    print('Число не является степенью двойки')
-
-
-# вариант 3
-def power_2(number):
-    k = 1
-    n = 0
-    if number <= 0:
-        return (False)
-    while k <= number:
-        if k == number:
-            return (True)
-        k = k * 2
-        n += 1
-
-    else:
-        return (False)
-
-
-number = int(input("Введите число number для проверки - является ли число number степенью двойки:"))
-result = power_2(number)
-
-if result == True:
-    print("Число ", number, " является степенью двойки, потому что 2 в степени", n, "является числом ", number)
-else:
-    print("Число", number, "не является степенью двойки")
