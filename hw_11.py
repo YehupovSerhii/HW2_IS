c33634420560f065a5cfd986fb7926c3eca6152b
YehupovSerhii <0676337867s@gmail.com>
2023-01-04 16:12:32 +0200

    homework_11 new

--- hw_11.py
+++ hw_11.py
@@ -1,35 +1,26 @@
-# 11.
-# Написать функцию, которая возвращает случайную строку заданной длины.
-# Строка должна состоять из больших и маленьких латинских букв и цифр.
-# def get_random_string(length: int) -> str:
-#   pass
-# Ограничения:
-# - Не использовать модуль string
-# - Не создавать руками список ['a', 'b', 'c', ..., 'X', 'Y', 'Z', 0, 1, ..., 8, 9]
-
 import random
+
+
 def get_random_string(length: int) -> str:
     small_letters = list(map(chr, range(97, 123)))
     big_letters = list(map(lambda char: char.upper(), small_letters))
     digits = list(range(0, 10))
     total = small_letters + big_letters + digits
-    # print(type(total))
-    # n=int(input())
     string_1 = ""
     for i in range(0, length):
         k = random.choice(total)
         string_1 = string_1 + str(k)
-    # print(string_1)
     return string_1
+
+
 while True:
     try:
-        n = float(
+        n = int(
             input("Введите длину строки для вывода случайных больших и маленьких букв латинского алфавита и цифр, n: "))
     except:
         print('Введите число!')
     else:
-        if float.is_integer(n) and n > 0:
-            n = int(n)
+        if n > 0:
             break
         else:
             print("Пробуйте еще раз ")
