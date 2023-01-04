c2e0652d3db2875dfb186d65883e22233a792d0c
YehupovSerhii <0676337867s@gmail.com>
2023-01-04 14:14:01 +0200

    homework_13

---
+++ hw_13.py
@@ -0,0 +1,36 @@
+# 13.Написать функцию, которая принимает несколько итерируемых объектов,
+# и возвращает список из кортежей составленных из элементов итерируемых объектов одного индекса.
+# Функция также должна принимать параметры с дефолтными значения:
+# full=False - по умолчанию "склеить" последовательности по кратчайшей, иначе по самой длинной
+# default=None - если full=True, вместо недостающих элементов поставить значение указанное в параметре default
+# seq1 = [1, 2, 3, 4, 5]
+# seq2 = [9, 8, 7]
+# custom_zip(seq1, seq2) -> [(1, 9), (2, 8), (3, 7)]
+# custom_zip(seq1, seq2, full=True, default="Q") -> [(1, 9), (2, 8), (3, 7), (4, 'Q'), (5, 'Q')]
+# Встроенную функцию zip не используем.
+# def custom_zip(*sequences: Iterable, full=False, default=None) -> List[Tuple]:
+#   pass
+# """
+
+def custom_zip(*sequences, full=False, default=None):
+    sequences = list(sequences)
+    if full:
+        sequences.sort(key=len, reverse=True)
+    else:
+        sequences.sort(key=len)
+    sq_count = len(sequences)
+    sq_start = sequences[0]
+    res = []
+    for cnt, item in enumerate(sq_start):
+        tup = ()
+        for i in range(0,sq_count):
+            if full and len(sequences[i]) < cnt+1:
+                tup = tup + (default,)
+            else:
+                tup = tup + (sequences[i][cnt],)
+        res.append(tup)
+    return res
+
+print(custom_zip([1,2,3], [4,5,6,7], [8,9]))
+print(custom_zip([1, 2, 3, 4, 5], [9, 8, 7]))
+print(custom_zip([1, 2, 3, 4, 5], [9, 8, 7], full=True, default="Q"))
